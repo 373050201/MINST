a73fa690c130a03aefe768fac27f1bb46a2de784
wyw <15201538267@163.com>
2026-05-20 13:33:48 +0800

    new commit

--- cnn/test.py
+++ cnn/test.py
@@ -29,7 +29,7 @@ for item in test_loader:
     imgs=imgs.cuda()
     targets=targets.cuda()
 
-    outputs,_,_,_,_=myModel(imgs)
+    outputs=myModel(imgs)
     correct=(outputs.argmax(1)==targets).sum()
     total_correct += correct
 accuracy=total_correct/len(test_data)
@@ -42,32 +42,33 @@ for i in range(0,show_num):
     img_np=img.squeeze(0).numpy()#转换为numpy方便展示
     img=img.unsqueeze(0)
     img=img.cuda()
-    output,conv1_output,pool1_output,conv2_output,pool2_output=myModel(img)
+    output=myModel(img)
+    # output,conv1_output,pool1_output,conv2_output,pool2_output=myModel(img)
 
-    #展示中间层输出
-    fig,([ax1,ax2],[ax3,ax4])=plt.subplots(2,2,figsize=(8,8))
-    #conv1+ReLU
-    grid_conv1=make_grid(conv1_output[0].cpu().unsqueeze(1),nrow=8,normalize=True)#排成网格展示
-    ax1.imshow(grid_conv1.permute(1,2,0))#CHW转HWC
-    ax1.set_title("conv1+ReLU")
-    ax1.axis('off')
-    #maxPool1
-    grid_pool1 = make_grid(pool1_output[0].cpu().unsqueeze(1), nrow=8, normalize=True)  # 排成网格展示
-    ax2.imshow(grid_pool1.permute(1,2,0))  # CHW转HWC
-    ax2.set_title("max_pool1")
-    ax2.axis('off')
-    #conv2+ReLU
-    grid_conv2=make_grid(conv2_output[0].cpu().unsqueeze(1),nrow=8,normalize=True)
-    ax3.imshow(grid_conv2.permute(1,2,0))
-    ax3.set_title("conv2+ReLU")
-    ax3.axis('off')
-    #maxPool2
-    grid_pool2=make_grid(pool2_output[0].cpu().unsqueeze(1),nrow=8,normalize=True)
-    ax4.imshow(grid_pool2.permute(1,2,0))
-    ax4.set_title("max_pool2")
-    ax4.axis('off')
-    plt.tight_layout()
-    plt.show()
+    # #展示中间层输出
+    # fig,([ax1,ax2],[ax3,ax4])=plt.subplots(2,2,figsize=(8,8))
+    # #conv1+ReLU
+    # grid_conv1=make_grid(conv1_output[0].cpu().unsqueeze(1),nrow=8,normalize=True)#排成网格展示
+    # ax1.imshow(grid_conv1.permute(1,2,0))#CHW转HWC
+    # ax1.set_title("conv1+ReLU")
+    # ax1.axis('off')
+    # #maxPool1
+    # grid_pool1 = make_grid(pool1_output[0].cpu().unsqueeze(1), nrow=8, normalize=True)  # 排成网格展示
+    # ax2.imshow(grid_pool1.permute(1,2,0))  # CHW转HWC
+    # ax2.set_title("max_pool1")
+    # ax2.axis('off')
+    # #conv2+ReLU
+    # grid_conv2=make_grid(conv2_output[0].cpu().unsqueeze(1),nrow=8,normalize=True)
+    # ax3.imshow(grid_conv2.permute(1,2,0))
+    # ax3.set_title("conv2+ReLU")
+    # ax3.axis('off')
+    # #maxPool2
+    # grid_pool2=make_grid(pool2_output[0].cpu().unsqueeze(1),nrow=8,normalize=True)
+    # ax4.imshow(grid_pool2.permute(1,2,0))
+    # ax4.set_title("max_pool2")
+    # ax4.axis('off')
+    # plt.tight_layout()
+    # plt.show()
 
     predict=output.argmax(1).item()#预测值
     print(f"当前预测值为{predict}，目标值为{target}")
